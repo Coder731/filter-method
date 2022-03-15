ad5c327f7dac405e3a0fff42500be8d45f41cfc8
Coder731 <stephenobrien1@gmail.com>
2022-03-15 10:22:32 +0000

    TotalGroupbyObjectProperty Add teamMember to array
    Total andGroup by
    a specific
    Object Property
    
    Add teamMember to array

--- reduce2.js
+++ reduce2.js
@@ -32,10 +32,45 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Mark',
+    profession: 'Manager',
+    yrsExperience: 10
   }
 ];
 
+// Part 2:
 // Totaling a specific object property
 
 
+// only difference here to  above is that its executing on an object now instead of an individual number.
+// so instead of adding the elements up
+// we need to dive into it using dot notation to 
+// get the yrsExperience
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0); // we are just adding 
+                                                                        // current.yrsExperience to the accumulator 
+                                                                        // on each pass of the callback function
+                                                                        // NOTE: if do not specify initial value
+                                                                        // it tries to add an object and an integer together
+                                                                        // end up with unusual
+                                                                        //  object string integer hybrid
+                                                                        // not good
+                                                                        // so alsways specify initial value.
+console.log(totalExperience);
+
 // Grouping by a property, and totaling it too
+
+// what we want:
+// {Developer: 12, Designer: 4}
+let experienceByProfession = teamMembers.reduce((acc, curr) => {// first set of braces is the boundary of the callback function
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {}); // second set of braces, after the comma is our inital value, an empty object.  (This is what the accumulator will be on the first pass.)
+
+console.log(experienceByProfession);
